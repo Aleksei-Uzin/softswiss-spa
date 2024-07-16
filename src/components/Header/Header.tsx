89b7590604d86a1be4d21da2f0c3a4ed8dbcf50a
Aleksei-Uzin <aleksei.uzin@gmail.com>
2024-07-16 23:46:54 +0400

    add styles to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
+import Box from '../Box'
 import Title from '../Title'
 import Button from '../Button'
 import './styles.scss'
 
 export function Header() {
   return (
-    <header className="header">
-      <Title type="h1">Discover the vast expanses of space</Title>
-      <p>Where the possibilities are endless!</p>
-      <Button className="header_button">Learn more</Button>
-    </header>
+    <Box className="header">
+      <header>
+        <Title type="h1">
+          Discover the vast
+          <br />
+          expanses of <span className="header_title_tail-pink">space</span>
+        </Title>
+        <p className="header_content">
+          Where the possibilities are{' '}
+          <span className="header_title_tail-yellow">endless!</span>
+        </p>
+        <Button className="header_button">Learn more</Button>
+      </header>
+    </Box>
   )
 }
