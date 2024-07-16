bfd250a9637464bb8dac0850b75b9d5a499c908b
Aleksei-Uzin <aleksei.uzin@gmail.com>
2024-07-16 09:23:21 +0400

    init: app entry point

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Footer, Header, Main, Navigation } from './components'
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
